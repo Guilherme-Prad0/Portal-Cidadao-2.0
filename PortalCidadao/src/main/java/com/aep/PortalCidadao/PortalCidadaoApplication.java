b661a42585e707972ee426b892ff5946134d2bcb
Guilherme <guilhermetprado@gmail.com>
2026-06-03 19:33:19 -0300

    commit

---
+++ PortalCidadao/src/main/java/com/aep/PortalCidadao/PortalCidadaoApplication.java
@@ -0,0 +1,13 @@
+package com.aep.PortalCidadao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalCidadaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalCidadaoApplication.class, args);
+	}
+
+}
---
+++ PortalCidadao/src/test/java/com/aep/PortalCidadao/PortalCidadaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aep.PortalCidadao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalCidadaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
